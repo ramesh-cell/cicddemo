cb7aef81ef4c26d2520323cb178446f69215429f
rames <rameshjava4plus@gmail.com>
2025-07-06 12:54:47 +0530

    added code

--- src/test/java/com/test/cicddemo/CicddemoApplicationTests.java
+++ src/test/java/com/test/cicddemo/CicddemoApplicationTests.java
@@ -2,9 +2,13 @@ package com.test.cicddemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class CicddemoApplicationTests {
+
+	@MockitoBean
+	private UserService userService;
 
 	@Test
 	void contextLoads() {
